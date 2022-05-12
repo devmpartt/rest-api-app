2db4a818d30208bf34537cf508b64af9e0635005
devmpartt <79589958+devmpartt@users.noreply.github.com>
2022-05-12 14:59:06 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+const API_URL = 'http://www.omdbadi.com?apikey=eba88d20';
+
+const App = () => {
+
+  const seachMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+  }
+
+  useEffect(() => {
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>App </h1>
   );
 }
 
